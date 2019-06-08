4cfc948c150ba63fc154961d1c0b5f5f28598b31
DESKTOP-J5SHQH0\Юрій <yura.gusak7@gmail.com>
2019-06-08 20:09:32 +0300

    added profile page logic

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { IUser } from '../models/user.interface';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  userState: firebase.User;
+
+  constructor(private afAuth: AngularFireAuth, private router: Router, private toastController: ToastController) {
+    this.afAuth.authState.subscribe((user: firebase.User) => {
+      this.userState = user;
+    })
+  }
 
   ngOnInit() {
   }
 
+  async deleteAccount() {
+    var user = firebase.auth().currentUser;
+    const toast = await this.toastController.create({
+      header: 'Really want to delete account?',
+      position: 'bottom',
+      buttons: [
+        {
+          side: 'start',
+          text: 'Yes',
+          handler: () => {
+            user.delete().then(() => {
+              this.router.navigate(['login'], { replaceUrl: true })
+            })
+              .catch((error) => {
+                console.log(error);
+              })
+          }
+        }, {
+          text: 'No',
+          role: 'cancel'
+        }
+      ]
+    });
+    toast.present();
+  }
+
+  async signOut() {
+    const toast = await this.toastController.create({
+      header: 'Really want to sign out?',
+      position: 'bottom',
+      buttons: [
+        {
+          side: 'start',
+          text: 'Yes',
+          handler: () => {
+            this.afAuth.auth.signOut();
+            this.router.navigate(['login'], { replaceUrl: true });
+          }
+        }, {
+          text: 'No',
+          role: 'cancel'
+        }
+      ]
+    });
+    toast.present();
+  }
 }
