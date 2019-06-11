7c080e12bc5246b34c03a5eeceb909962ef79521
DESKTOP-J5SHQH0\Юрій <yura.gusak7@gmail.com>
2019-06-11 18:08:24 +0300

    camera & background mode for update photo

--- src/app/profile/profile-update/profile-update.component.ts
+++ src/app/profile/profile-update/profile-update.component.ts
@@ -5,6 +5,8 @@ import { IUser } from '../../models/user.interface';
 import { timer, Observable } from 'rxjs';
 import { IUserCategories } from '../../models/user.categories';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { BackgroundMode } from '@ionic-native/background-mode/ngx'
 @Component({
   selector: 'app-profile-update',
   templateUrl: './profile-update.component.html',
@@ -16,9 +18,11 @@ export class ProfileUpdateComponent implements OnInit {
   constCategoriesNames:string[] = [];
   loadingUser = false;
 
+  photoToFireStore: any;
+
   selectedSegment = 'const';
 
-  constructor(private afs: AngularFirestore) {
+  constructor(private afs: AngularFirestore, private camera: Camera, private backgroundMode: BackgroundMode) {
 /*     let userEmail = localStorage.getItem('userEmail');
     this.afs.collection('usersMoneyFlow', ref => ref.where('userEmail', '==', userEmail))
       .valueChanges().subscribe((users: IUser[]) => {
@@ -42,6 +46,30 @@ export class ProfileUpdateComponent implements OnInit {
           })
         });
       })
+  }
+
+  takePhoto() {
+    this.backgroundMode.enable();
+    const options: CameraOptions = {
+      quality: 100,
+      targetHeight: 400,
+      targetWidth: 400,
+      correctOrientation: true,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      allowEdit: false
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.photoToFireStore = base64Image;
+    }).catch((err) => {
+      console.log("Error: ", err);
+    });
+
+    this.backgroundMode.disable();
   }
 
 }
