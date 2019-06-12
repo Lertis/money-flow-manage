9f1768b318140bdfcb9a649ff2ab6990a8ff702c
DESKTOP-J5SHQH0\Юрій <yura.gusak7@gmail.com>
2019-06-12 11:10:46 +0300

    profile card update

--- src/app/profile/profile-card/profile-card.component.ts
+++ src/app/profile/profile-card/profile-card.component.ts
@@ -3,6 +3,9 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 import { ToastController } from '@ionic/angular';
+import { timer } from 'rxjs';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { IUser } from '../../models/user.interface';
 
 @Component({
   selector: 'app-profile-card',
@@ -11,14 +14,23 @@ import { ToastController } from '@ionic/angular';
 })
 export class ProfileCardComponent implements OnInit {
   userState: firebase.User;
+  userPhotoUrl;
 
-  constructor(private afAuth: AngularFireAuth, private router: Router, private toastController: ToastController) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private toastController: ToastController,
+    private afs: AngularFirestore
+  ) {
     this.afAuth.authState.subscribe((user: firebase.User) => {
       this.userState = user;
     })
+    this.afs.collection('usersMoneyFlow').doc('c1ea4345-45b4-59e8-b9b7-2c1691644200').valueChanges().subscribe((user: IUser) => {
+      this.userPhotoUrl = user.userPhoto;
+    })
   }
   ngOnInit() {
-    
+  
   }
 
   async deleteAccount() {
