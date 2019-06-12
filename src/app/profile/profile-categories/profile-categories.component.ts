d3638e68fc31c97e7f61be5d1f77c2b55a07d2f3
DESKTOP-J5SHQH0\Юрій <yura.gusak7@gmail.com>
2019-06-13 00:30:35 +0300

    filter added by user categories, animation

--- src/app/profile/profile-categories/profile-categories.component.ts
+++ src/app/profile/profile-categories/profile-categories.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { IUser } from '../../models/user.interface';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-profile-categories',
   templateUrl: './profile-categories.component.html',
   styleUrls: ['./profile-categories.component.scss'],
+  animations: [
+    trigger('loadCategories', [
+      state('in', style({})),
+      transition('void => *', [
+        style({ height: 0, opacity: 0 }),
+        animate(400, style({ height: '*', opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class ProfileCategoriesComponent implements OnInit {
 
-  constructor() { }
+  allUserCategories = [];
 
-  ngOnInit() {}
+  constructor(private afs: AngularFirestore) {
+  }
+
+  ngOnInit() {
+    this.afs.collection('usersMoneyFlow').doc('c1ea4345-45b4-59e8-b9b7-2c1691644200').valueChanges().subscribe((res: IUser) => {
+      // this.allUserCategories = res.categories.categoriesList; //all
+
+      this.allUserCategories = res.categories.categoriesList.filter((data) => {
+       return data.canBeDeleted === true
+      });
+      // can be deleted === added by user
+
+    })
+  }
 
 }
