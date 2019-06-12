70bcf1810012a8c24d0fd85f6837fff896cf54af
DESKTOP-J5SHQH0\Юрій <yura.gusak7@gmail.com>
2019-06-12 21:04:18 +0300

    checks list

--- src/app/checks/checks-list/checks-list.component.ts
+++ src/app/checks/checks-list/checks-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { IUser } from '../../models/user.interface';
 import { timer } from 'rxjs';
+import { ICheck } from '../../models/check.interface';
 
 @Component({
   selector: 'app-checks-list',
@@ -9,6 +10,8 @@ import { timer } from 'rxjs';
   styleUrls: ['./checks-list.component.scss'],
 })
 export class ChecksListComponent implements OnInit {
+
+  currentUserId = '';
 
   sortBySelected: string;
 
@@ -25,19 +28,35 @@ export class ChecksListComponent implements OnInit {
 
   userCategories = [];
 
+  userChecks: ICheck[] = [];
+  filteredOrSorted: ICheck[] = [];
+
+  spinOldest = true;
+  spinNewest = true;
+  spinTime = true;
+
   constructor(private afs: AngularFirestore) {
     this.afs.collection('usersMoneyFlow', ref => ref.where('userEmail', '==', localStorage.getItem('userEmail'))).valueChanges()
     .subscribe((users: IUser[]) => {
       users.forEach((each: IUser) => {
+        this.currentUserId = each.userId;
         this.userCategories = each.categories.categoriesList;
       })
+    })
+
+    this.afs.collection('usersMoneyFlow').doc(localStorage.getItem('userId')).collection('checks').valueChanges().subscribe((checks: ICheck[]) => {
+      this.userChecks = checks;
     })
   }
 
   ngOnInit() {
-/*     timer(20000).subscribe(() => {
-      this.selectedFilterValue = 'selected';
-    }) */
+/*    timer(2000).subscribe(() => {
+      console.log('Category: ', this.filteredOrSorted = this.userChecks.filter((data) => data.category === 'Домашні улюбленці'));
+    })  */
+
+    timer(2000).subscribe(() => {
+      console.log(this.filteredOrSorted = this.userChecks.filter((data) => data.category === 'Домашні улюбленці'));
+    }) 
   }
 
   ionViewDidLoad() {
@@ -62,6 +81,10 @@ export class ChecksListComponent implements OnInit {
   changeRadioButton($event) {
     console.log($event)
     this.selectedFilterValue = $event.detail.value;
+  }
+
+  changeRadioButtonInCategories($event) {
+    console.log($event)
   }
 
 }
